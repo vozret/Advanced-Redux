c46dd599ab105d6f274c8d744010fa36c8eed758
Veronika Ozretic <vozret00@fesb.hr>
2021-06-09 16:00:34 +0200

    getting started with fetching data (259)

---
+++ src/store/cart-actions.js
@@ -0,0 +1,82 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://react-redux-21461-default-rtdb.firebaseio.com/cart.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error...",
+          message: "Fetching cart data failed",
+        })
+      );
+    }
+  };
+};
+
+// this is action creator
+export const sendCartData = (cart) => {
+  // it returns a function which eventually returns an action
+  // we are not running this in areducer so we can use async code
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-redux-21461-default-rtdb.firebaseio.com/cart.json",
+        {
+          // with PUT method we store the data as it is
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success...",
+          message: "Sent cart data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error...",
+          message: "Sending cart data failed",
+        })
+      );
+    }
+  };
+};
