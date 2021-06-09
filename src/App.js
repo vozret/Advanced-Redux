361a1c798c259a400c20ff84e531fb99dd8b3230
Veronika Ozretic <vozret00@fesb.hr>
2021-06-09 14:54:31 +0200

    useEffect with Redux (255) -> fat reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -6,6 +7,19 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  // useSelector sets a subscription to redux
+  // and every time the state changes, "cart" will also change
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    // we are first updating the state and then sending the data to the server
+    // thus creating a fat reducer
+    fetch("https://react-redux-21461-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
